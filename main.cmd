948beafae5c34e68951e9decd1a2de9d6df2a18e
TheEffectik <andruxa-mix112@yandex.ru>
2023-04-02 18:23:39 +0300

    checker

---
+++ main.cmd
@@ -0,0 +1,87 @@
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
+echo %date% %time%
+python a.py
+timeout 1200
