5d78c5c091243912d8244b52f3cdfc39ce4b2f17
TheEffectik <andruxa-mix112@yandex.ru>
2023-04-02 18:38:35 +0300

    xd

--- a.py
+++ a.py
@@ -4,21 +4,24 @@ import time
 import difflib
 import os
 import filecmp
-# n = int(input("Количество сайтов"))
 
-# print("Введите сайт - название")
 requests.Response.encoding = 'utf-8'
-url = ['https://neerc.ifmo.ru/school/ioip/index.html',
-       'https://cups.online/ru/rounds/767/leaderboard',
-       'https://olymp.msu.ru/rus/event/7710/page/3118',
-       'https://olymp.mephi.ru/rosatom/winners']
 
-names = ['ИОИП', 'ТК', 'ЛОМ', 'росатом']
-f = [0].copy() * len(names)
-n = len(names) - 1
-delay = 20 * 60 # check once a day
+url = []
+names = []
+
+with open('links.txt', 'r', encoding="utf-8") as mn:
+    for i in mn.readlines():
+        print(i)
+        lnk, name = map(str, i.split())
+        url.append(lnk)
+        names.append(name)
+    
+
+n = len(names)
+
 # <- чекпоинт 
-# n = len(names)
+
 # for i in range(n):
 #     response = requests.get(url[i])
 #     response.encoding = response.apparent_encoding
@@ -28,10 +31,6 @@ delay = 20 * 60 # check once a day
 
 
 for i in range(n):
-    if f[i] == 1: 
-        print(f'{names[i]} +++++')
-        continue
-
     response = requests.get(url[i])
     if response.status_code == 200:
         response.encoding = response.apparent_encoding
